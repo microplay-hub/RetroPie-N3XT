f9d438911e5357f6fed7df8376943a30293ea095
Fredy <44968046+Liontek1985@users.noreply.github.com>
2023-11-11 10:50:53 +0100

    Update lr-flycast-dev.sh  fix atomis naomi

--- scriptmodules/libretrocores/lr-flycast-dev.sh
+++ scriptmodules/libretrocores/lr-flycast-dev.sh
@@ -58,7 +58,7 @@ function install_lr-flycast-dev() {
 function configure_lr-flycast-dev() {
     local sys
     local def
-    for sys in "arcade" "dreamcast"; do
+    for sys in "arcade" "atomiswave" "naomi" "dreamcast"; do
         def=0
         isPlatform "kms" && [[ "$sys" == "dreamcast" ]] && def=1
         # segfaults on the rpi without redirecting stdin from </dev/null
