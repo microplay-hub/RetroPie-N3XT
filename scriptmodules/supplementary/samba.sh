0b11e5705ead637d160579b5f2a0393ee6010494
Fredy <44968046+Liontek1985@users.noreply.github.com>
2023-11-17 14:25:39 +0100

    Update samba.sh - add es musics

--- scriptmodules/supplementary/samba.sh
+++ scriptmodules/supplementary/samba.sh
@@ -50,6 +50,7 @@ function install_shares_samba() {
     add_share_samba "bios" "$home/RetroPie/BIOS"
     add_share_samba "configs" "$configdir"
     add_share_samba "splashscreens" "$datadir/splashscreens"
+    add_share_samba "musics" "$datadir/musics"
 
     # Add `wsdd` so that RetroPie is easily discovered by Windows clients
     # Only available on Debian 12/Ubuntu 22.04 and later
